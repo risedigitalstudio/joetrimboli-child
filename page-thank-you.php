39e15ccf53ee0d8fb7a0b6a1782fbaabc71be6e6
Nicole Tseronakis <nicole@risedigitalstudio.ca>
2022-02-04 21:29:23 -0500

    404

---
+++ page-thank-you.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+<section class="page-wrap sec-pad">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+				<?php
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'loop-templates/content', 'page' );
+				}
+				?>
+               <p><a href="<?php echo site_url();?>/account" class="default-btn">Go to Account</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
